6eeea012a607d29ef0843bc4adb6ffa4624b3f51
Soham2020 <majumder70.ranu@gmail.com>
2023-07-28 12:22:40 +0530

    Made inside a same class

--- src/Task2/Subjects.java
+++ src/Task2/Subjects.java
@@ -4,26 +4,27 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Subjects {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\2282058\\Downloads\\eclipse-java-2023-03-R-win32-x86_64 (1)\\eclipse\\MiniProject\\driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("https://openlibrary.org/");
+	
+	private static String url = "https://openlibrary.org/";
+	
+	public static Boolean checkCountBooks (WebDriver driver) {
+		driver.navigate().to(url);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Click on Browse button
 		WebElement browse = driver.findElement(By.className("browse-component"));
 		browse.click();
-		
+
 		/*
 		 * There is select tag to handle it by using Select class
 		 * First tired by storing into a list of webElement, but each elements are not accessible because there is no common property in between the list elements and list is required a common property
@@ -32,6 +33,7 @@ public class Subjects {
 		 */
 		WebElement browse1 = driver.findElement(By.xpath("//*[@id=\"header-bar\"]/ul[1]/li[2]/div/details/div/ul/li[1]/a"));
 		browse1.click();
+
 		
 //		Go to see more
 		WebElement seeMore = driver.findElement(By.linkText("See more..."));
@@ -62,16 +64,41 @@ public class Subjects {
 		
 //		Check whether it is more than 1000
 		if(Integer.parseInt(temp) > 1000)
-			System.out.println(true);
-		else {
-			System.out.println(false);
-		}
+			return true;
 		
-		
-//		Closing the driver
-		driver.close();
-		
+		return false;
+	}
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+		switch (option) {
+		case 1: {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\2282058\\Downloads\\eclipse-java-2023-03-R-win32-x86_64 (1)\\eclipse\\MiniProject\\driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			
+			Boolean result = checkCountBooks(driver);
+			System.out.println(result);			
+//			Closing the driver
+			driver.close();
+			break;
+		}
+		case 2: {
+			System.setProperty("webdriver.edge.driver", "C:\\Users\\2282058\\Downloads\\eclipse-java-2023-03-R-win32-x86_64 (1)\\eclipse\\MiniProject\\driver\\msedgedriver.exe");
+			driver = new EdgeDriver();
+			driver.navigate().to(url);
+
+			Boolean result = checkCountBooks(driver);
+			System.out.println(result);			
+//			Closing the driver
+			driver.close();
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + option);
+		}
 
 	}
 
